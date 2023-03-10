dde8f447929398d67bf2524855e27726f1b7899a
Maciej Chyży <macchyzy@gmail.com>
2023-03-10 15:48:12 +0100

    added proper functions to .js file

--- js/script.js
+++ js/script.js
@@ -1,38 +1,47 @@
-let selectElement = document.querySelector(".js-select");
-let inputElement = document.querySelector(".js-input");
-let convertingButton = document.querySelector(".js-convertingButton");
-let resultElement = document.querySelector(".js-resultElement");
+{
+    const calculateResult = () => {
 
-convertingButton.addEventListener("click", (event) => {
-    event.preventDefault();
+        const selectElement = document.querySelector(".js-select");
+        const inputElement = document.querySelector(".js-input");
 
-    let result;
+        if (!inputElement.value) {
+            return;
+        }
 
-    if (!inputElement.value) {
-        return;
+        switch (selectElement.value) {
+            case "EUR":
+                return inputElement.value / 4.79;
+
+            case "USD":
+                return inputElement.value / 4.45;
+
+            case "GBP":
+                return inputElement.value / 5.36;
+
+
+            case "CZK":
+                return inputElement.value / 0.2;
+
+            case "SEK":
+                return inputElement.value / 0.43;
+        }
     }
 
-    switch (selectElement.value) {
-        case "EUR":
-            result = inputElement.value / 4.79;
-            break;
+    const showResult = () => {
 
-        case "USD":
-            result = inputElement.value / 4.45;
-            break;
+        const convertingButton = document.querySelector(".js-convertingButton");
 
-        case "GBP":
-            result = inputElement.value / 5.36;
-            break;
+        convertingButton.addEventListener("click", (event) => {
+            event.preventDefault();
 
-        case "CZK":
-            result = inputElement.value / 0.2;
-            break;
+            calculateResult();
 
-        case "SEK":
-            result = inputElement.value / 0.43;
-            break;
+            const resultElement = document.querySelector(".js-resultElement");
+            const selectElement = document.querySelector(".js-select");
+            const inputElement = document.querySelector(".js-input");
+
+            resultElement.innerHTML = `<strong>${inputElement.value} PLN = ${calculateResult().toFixed(2)} ${selectElement.value}</strong>`;
+        });
     }
-
-    resultElement.innerHTML = `<strong>${inputElement.value} PLN = ${result.toFixed(2)} ${selectElement.value}</strong>`;
-});
+    showResult();
+}
